ad91b00907cfba9c408b0d77c3518dc6be4fcea4
NorthernMan54 <buyminivan-northern@yahoo.ca>
2018-03-04 18:12:07 -0500

    Manufacturer is plugin name

--- index.js
+++ index.js
@@ -155,7 +155,7 @@ DhtAccessory.prototype = {
     var informationService = new Service.AccessoryInformation();
 
     informationService
-      .setCharacteristic(Characteristic.Manufacturer, "NorthernMan54")
+      .setCharacteristic(Characteristic.Manufacturer, "dht22")
       .setCharacteristic(Characteristic.Model, this.service)
       .setCharacteristic(Characteristic.SerialNumber, hostname + "-" + this.name)
       .setCharacteristic(Characteristic.FirmwareRevision, require('./package.json').version);
