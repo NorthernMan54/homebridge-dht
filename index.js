507e702abdf2acd7c3ce65157f30a2ae1c082301
NorthernMan54 <buyminivan-northern@yahoo.ca>
2019-05-12 11:12:12 -0400

    Fix for device types

--- index.js
+++ index.js
@@ -65,6 +65,8 @@ function DhtAccessory(log, config) {
   this.refresh = config.refresh || "60"; // Every minute
   this.storage = config['storage'] || "fs";
 
+  this.type = parseInt(this.service);
+
   // dhtExec = config.dhtExec || "dht22";
   cputemp = config.cputemp || "cputemp";
 
@@ -78,7 +80,7 @@ function DhtAccessory(log, config) {
 DhtAccessory.prototype = {
 
   getDHTTemperature: function(callback) {
-    sensor.read(22, this.gpio, function(err, temperature, humidity) {
+    sensor.read(this.type, this.gpio, function(err, temperature, humidity) {
       if (!err) {
         this.log("DHT Status: %s, Temperature: %s°C, Humidity: %s%", 0, roundInt(temperature), roundInt(humidity));
         this.log_event_counter = this.log_event_counter + 1;
